8fa9be1c565c7ec3acb1eaeb033252f2dbd65585
VicotheGaymer <vinimaxboy@gmail.com>
2024-11-13 13:53:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -39,10 +39,128 @@ async function cadastrarAnimal()
     } catch (error) {
         console.error('Erro ao cadastrar animal:', error);
     }
+
+    const fform = document.getElementById('fform');
+    fform.innerHTML = '';
 }
 
-function apagarLista()
+async function apagarLista()
 {
     const animalList = document.getElementById('animalList');
     animalList.innerHTML = '';
+}
+
+async function apagaItem() {
+    const fform = document.getElementById('fform');
+    fform.innerHTML = `
+        <div>
+            <form>
+                <label>Qual o Id do Animal:</label>
+                <input type="number" id="animalId" required>
+            </form>
+            <button onclick="deletaAnimal()">Confirmar</button>
+        </div>`;
+}
+
+async function deletaAnimal() {
+    const ID = document.getElementById('animalId').value;
+
+    try {
+        await fetch(`${apiUrl}/${ID}`, {
+            method: 'DELETE',
+        });
+    } catch (error) {
+        console.error('Erro ao deletar animal:', error);
+    }
+
+    const fform = document.getElementById('fform');
+    fform.innerHTML = '';
+}
+
+async function atualizaItem() {
+    const fform = document.getElementById('fform');
+    fform.innerHTML = `
+        <div>
+            <form>
+                <div>
+                    <label>Qual o Id do Animal:</label>
+                    <input type="number" id="animalId" required>
+                </div>
+                <div>
+                    <label>Nome:</label>
+                    <input type="text" id="atualizaNome">
+                </div>
+                <div>
+                    <label>Idade:</label>
+                    <input type="number" id="atualizaIdade">
+                </div>
+                <div>
+                    <label>Raça:</label>
+                    <input type="text" id="atualizaRaca">
+                </div>
+                    </form>
+            <button onclick="atualizaAnimal()">Confirmar</button>
+        </div>`;
+}
+
+async function atualizaAnimal() {
+    const ID = document.getElementById('animalId').value;
+    const updatedAnimal = {};
+
+    const nome = document.getElementById("atualizaNome").value;
+    if (nome) {
+        updatedAnimal.Nome = nome;
+    }
+
+    const idade = document.getElementById("atualizaIdade").value;
+    if (idade) {
+        updatedAnimal.Idade = idade;
+    }
+
+    const raca = document.getElementById("atualizaRaca").value;
+    if (raca) {
+        updatedAnimal.Raca = raca;
+    }
+
+    if (Object.keys(updatedAnimal).length === 0) {
+        alert("Por favor, forneça pelo menos um campo para atualizar.");
+        return;
+    }
+
+    try {
+        await fetch(`${apiUrl}/${ID}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updatedAnimal)
+        });
+    } catch (error) {
+        console.error('Erro ao atualizar animal:', error);
+    }
+
+    const fform = document.getElementById('fform');
+    fform.innerHTML = '';
+}
+
+async function adicionaItem() {
+    const fform = document.getElementById('fform');
+    fform.innerHTML = `
+        <div>
+             <form>
+                <div>
+                    <label>Nome:</label>
+                    <input type="text" id="Nome">
+                </div>
+                <div>
+                    <label>Idade:</label>
+                    <input type="number" id="Idade">
+                </div>
+                <div>
+                    <label>Raça:</label>
+                    <input type="text" id="Raca">
+                </div>
+            </form>
+            <button onclick="cadastrarAnimal()">Confirmar</button>
+        </div>`;
 }
